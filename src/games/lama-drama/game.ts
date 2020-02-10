7f775d7769b671dfbe77a2f5a972cabc05c4f046
Jonathan barbeau <jobarbo@me.com>
2020-02-10 10:53:02 -0500

    linked new scenes in game.ts

--- src/games/lama-drama/game.ts
+++ src/games/lama-drama/game.ts
@@ -5,22 +5,32 @@
  */
 
 import "phaser";
-import { MainScene } from "./scenes/main-scene";
+import { BootScene } from "./scenes/boot-scene";
+import { GameScene } from "./scenes/game-scene";
+import { MainMenuScene } from "./scenes/main-menu-scene";
 
 // main game configuration
 const config: Phaser.Types.Core.GameConfig = {
+  title: "Lama Drama",
+  url: "https://github.com/digitsensitive/phaser3-typescript",
+  version: "0.01",
   width: 800,
   height: 600,
   type: Phaser.AUTO,
   parent: "game",
-  scene: MainScene,
+  scene: [BootScene, MainMenuScene, GameScene],
+  input: {
+    keyboard: true
+  },
   physics: {
     default: 'arcade',
     arcade: {
         gravity: { y: 300 },
         debug: true
     }
-},
+  },
+  backgroundColor: "#98d687",
+  render: { pixelArt: true, antialias: false }
 };
 
 export class Game extends Phaser.Game {
